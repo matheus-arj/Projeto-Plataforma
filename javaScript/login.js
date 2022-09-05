d262d1e6bfedd59888a49378b67b6937ed749c06
unknown <matheus.arj@hotmail.com>
2022-09-04 21:36:15 -0300

    pagina de login, atualizada com 'dicionario'

--- javaScript/login.js
+++ javaScript/login.js
@@ -1,12 +1,43 @@
-function logar(){
-  var email = document.getElementById("email");
-  var senha = document.getElementById("senha");
 
-  if (email.value == "admin@admin.com" && senha.value == "admin"){
+const users = [
+  {
+    email: "matheus.araujo2@hotmail.com", 
+    senha: "987654321"
+  },
+  {
+    email: "julia.silva@hotmail.com", 
+    senha: "123456789"
+  },
+  {
+    email: "marcela_almeida@gmail.com",
+    senha: "abc12345"
+  }
+
+]
+
+function logar(){
+  let email = document.getElementById("email").value;
+  let senha = document.getElementById("senha").value;
+
+  console.log("email",email)
+
+  const isUserValid = users.find(user => user.email === email && user.senha === senha)
+
+  console.log("user", isUserValid)
+
+  if(isUserValid) {
     localStorage.setItem("acesso", true);
     alert("Usuario autenticado!");
     window.location.href = "/index.html"
-  } else{
+  } else {
     alert("Usuário ou senha inválidos!")
   }
+
+  // if (email.value == "admin@admin.com" && senha.value == "admin"){
+  //   localStorage.setItem("acesso", true);
+  //   alert("Usuario autenticado!");
+  //   window.location.href = "/index.html"
+  // } else{
+  //   alert("Usuário ou senha inválidos!")
+  // }
 }
